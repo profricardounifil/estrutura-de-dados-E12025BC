3f24a592f37e968ed692a9805a6bb22f0e972aa7
ricardo <ricardo.petri@unifil.br>
2025-02-17 20:24:19 -0300

    Implementação Lista Simples

--- src/ListaSimples.java
+++ src/ListaSimples.java
@@ -12,6 +12,7 @@ public class ListaSimples {
         for (int i = 0; i < this.tamanho ; i++) {
             if(posicaoEstaVazia(i)) {
                 this.lista[i] = elemento;
+                break;
             }
         }
     }
@@ -30,6 +31,46 @@ public class ListaSimples {
     public void percorrerElementos() {
         for (int i = 0; i < this.tamanho; i++) {
             System.out.println(this.lista[i]);
+        }
+    }
+
+    public void removerPorIndice(int indice) {
+        if(verificarIntervalo(indice)) {
+            if(posicaoEstaVazia(indice)) {
+                System.out.println("A posição informada já está vazia.");
+            } else {
+                this.lista[indice] = null;
+            }
+        }
+    }
+
+    public boolean verificarIntervalo(int indice) {
+        if(indice >= 0 && indice <= this.tamanho-1) {
+            return true;
+        } else {
+            System.out.println("O indice informado está fora do intervalo da lista.");
+            return false;
+        }
+    }
+
+    public boolean buscaElemento(int elemento) {
+        for (int i = 0; i < this.tamanho; i++) {
+            if(this.lista[i] == elemento) {
+                System.out.println("O elemento " + elemento + " existe na lista, no índice " + (i+1));
+                return true;
+            }
+        }
+        System.out.println("O elemento não foi encontrado!");
+        return false;
+    }
+
+    public void editarPorIndice(int indice, int elemento) {
+        if(verificarIntervalo(indice)) {
+            if(posicaoEstaVazia(indice)) {
+                System.out.println("A posição informada já está vazia.");
+            } else {
+                this.lista[indice] = elemento;
+            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        ListaSimples listaSimples = new ListaSimples(6);
+        ListaSimples listaSimples = new ListaSimples(3);
+        listaSimples.inserirElemento(1);
+        listaSimples.inserirElemento(2);
+        listaSimples.inserirElemento(3);
         listaSimples.percorrerElementos();
-        listaSimples.inserirElemento(10);
+        listaSimples.editarPorIndice(2, -4);
         listaSimples.percorrerElementos();
     }
 }
